cf5be85c0ee7ed5edbe9649c74686142e0b8b2c2
agustim1964 <soclagusti@gmail.com>
2024-11-19 19:36:54 +0100

    Updates

--- assets/md.transition-legacy-4Epm8bo3.js
+++ assets/md.transition-legacy-4Epm8bo3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CmJ_0XlS.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BEwaQ4US.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-H5kAGupg.js
+++ assets/status-tap-legacy-H5kAGupg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CmJ_0XlS.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-BEwaQ4US.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
