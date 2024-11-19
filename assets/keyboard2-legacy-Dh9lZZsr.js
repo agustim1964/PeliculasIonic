e94b5640f4af0952c8ea51496b71162440423f6d
agustim1964 <soclagusti@gmail.com>
2024-11-19 20:56:21 +0100

    Updates

--- assets/keyboard2-legacy-Dh9lZZsr.js
+++ assets/keyboard2-legacy-Dh9lZZsr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CmJ_0XlS.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CLAmBFBy.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BYi2UcBi.js
+++ assets/swipe-back-legacy-BYi2UcBi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CmJ_0XlS.js","./index3-legacy-BTl0K35o.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CLAmBFBy.js","./index3-legacy-BNG-3iOQ.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
